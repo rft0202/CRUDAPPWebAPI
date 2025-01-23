5eb0a0b8b56236b91b5513a2faea87e6a2916795
rft0202 <114946327+rft0202@users.noreply.github.com>
2025-01-23 10:03:07 -0500

    finished crud routes

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const port = process.env.port||3000; //if not environment, default to 3000
 
 //Set up middleware to parse json requests
 app.use(bodyParser.json());
+//Added
+app.use(express.urlencoded({extended:true}));
 
 //MongoDB connection setup
 const mongoURI = "mongodb://localhost:27017/crudapp"; // slash database //default URI
@@ -25,22 +27,21 @@ db.once("open", ()=>{
     console.log("Connected to MongoDB Database");
 });
 
-//Added - Setup Mongoose Schema
+//Setup Mongoose Schema
 const peopleSchema = new mongoose.Schema({ //basic template for the data we're trying to retrieve from db
     firstname:String,
     lastname:String,
     email:String
 }); 
 
-//Added 
 const Person = mongoose.model("Person", peopleSchema, "peopledata"); //use Person to call mongoDB commands //something, schema, name of collection
 
-//Added - App Routes
+//App Routes
 app.get("/", (req,res)=>{
     res.send("Server is working");
 });
 
-//Added
+//Read Routes (GET)
 app.get("/people", async (req, res)=>{
     try{
         const people = await Person.find(); //call mongoDB to get data
@@ -49,6 +50,68 @@ app.get("/people", async (req, res)=>{
         res.status(500).json({error:"Failed to get people."});
     }
 });
+
+//Grab user by ID
+app.get("/people/:id", async (req, res)=>{
+    try{
+        //console.log(req.params.id);
+        const person = await Person.findById(req.params.id);
+        if(!person){ //if there isn't a person
+            return res.status(404).json({error:"Person not found."}); //not found, return out of function
+        }
+        res.json(person); //found, respond with person
+
+    }catch(err){
+        res.status(500).json({error:"Failed to get person."});
+    }
+});
+
+//Create Route (POST)
+app.post("/addperson", async (req, res)=>{
+    try{
+        const newPerson = new Person(req.body); //create new instance of a person
+        const savePerson = await newPerson.save(); //save the new person
+        res.status(201).json(savePerson);
+        console.log(savePerson); //log the data that got saved
+    } catch(err){
+        res.status(500).json({error:"Failed to add new person."});
+    }
+});
+
+//Update Route (PUT)
+app.put("/updateperson/:id", (req,res)=>{
+    //Example of a promise statement for async function (alternative to async and callback functions) (used by mongoose)
+    Person.findByIdAndUpdate(req.params.id, req.body, { //id, request body
+        new:true, //is a new request
+        runValidators:true 
+    }).then((updatedPerson)=>{ //if it completes, then (automatically pass into promise statement)
+        if(!updatedPerson){
+            return res.status(404).json({error:"Failed to find person."});
+        }
+        res.json(updatedPerson); //update person
+    }).catch((err)=>{ //function call
+        res.status(400).json({error:"Failed to update the person."}); 
+    }); 
+});
+
+//Delete Route (DELETE)
+app.delete("/deleteperson/firstname", async (req,res)=>{
+    try{
+        const personname = req.query; //query request
+        const person = await Person.find(personname); //find using the query
+
+        if(person.length === 0){ //=== means exactly equal to (the data type matches)
+            return res.status(404).json({error:"Failed to find the person."}); 
+        } 
+
+        const deletedPerson = await Person.findOneAndDelete(personname);
+        res.json({message: "Person deleted successfully."});
+
+    }catch(err){
+        console.log(err);
+        res.status(404).json({error:"Person not found."}); 
+    }
+}); 
 
 //Starts the server
 app.listen(port, ()=>{
